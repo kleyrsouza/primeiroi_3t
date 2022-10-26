adb5d459d7cab5874409ad27d0640247d99b1cc5
kleyrsouza <110063156+kleyrsouza@users.noreply.github.com>
2022-10-26 17:44:54 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -33,18 +33,22 @@ function quadrado(){
         document.write("O quadrado de " + i + " é " + (i*i)+"<br>");
     }
 }
-
+function moeda(atual){
+    return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+}
 function total(){
     let val = document.getElementById("valor").value;
     let ju = document.getElementById("juros").value;
     
     let t = document.getElementById("meses").value;
     let resultado = 0;
+    let saida +=
     for(let m = 1; m <= t; m++){
         resultado = (val * ((ju/100)+1));
         val = resultado;
-        document.write("Mês " + m + " valor de " + val + "<br>");
+        document.getElementById("mes").innerHTML =
+        //document.write("Mês " + m + " valor de " + moeda(val) + "<br>");
     }
-    document.write("Mês " + m + "valor de " + moeda(val) + "<br>");
+    document.getElementById("resultado").innerHTML = "total: " + moeda(resultado);
+    //document.write("O total é de: " + moeda(resultado) );
 }
-document.write("0 total é de: " + moeda(resultado) )
